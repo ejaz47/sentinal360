a2875ab52307851202f71276e4de579d7c44f8d6
Ejaz Ansari <ejaz.portal@gmail.com>
2023-12-23 19:19:12 -0500

    nunjuck added

---
+++ src/js/data.js
@@ -0,0 +1,3 @@
+module.exports = {
+    items: [{ title: 'Ejaz', id: 1 }, { title: 'barsddddddssssjvjvjvjssss', id: 2 }]
+};
--- webpack/loader/html-loader.js
+++ webpack/loader/html-loader.js
@@ -1,15 +1,34 @@
 import {getOptions, getCurrentRequest} from 'loader-utils';
 import fs from 'fs';
 import path from 'path';
+import nunjucks from 'nunjucks';
+
+const dataPath = path.resolve('./src/js/data.json'); // Adjust the path as needed
+
+const renderNunjucksTemplate = function (templateContent, data, templatePath) {
+    try {
+        const env = nunjucks.configure();
+        const renderedContent = nunjucks.renderString(templateContent, data);
+        return renderedContent;
+    }catch (ex) {
+        console.log(`Nunjuck Error: "${templatePath}" `, ex);
+        return '';
+    }
+};
 
 const insertIncludes = function (self, content, regex) {
     const includes = content.match(regex);
     let newContent = content;
     if (includes) {
+        const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
         includes.forEach((includeFilename) => {
             if(includeFilename.substr(0, 1) == '_'){
                 const includePath = path.resolve(`./src/html/${includeFilename}`);
                 let includeContent = fs.readFileSync(includePath, 'utf8');
+
+                // Add Nunjucks rendering here
+                includeContent = renderNunjucksTemplate(includeContent, data, includePath);
+
                 includeContent = insertIncludes(self, includeContent, regex);
                 newContent = newContent.replace(`<include>${includeFilename}</include>`, includeContent);
                 self.addDependency(includePath);
@@ -22,6 +41,7 @@ const insertIncludes = function (self, content, regex) {
 const loader = function (source) {
     const options = getOptions(this);
     const regex = new RegExp(`(${options.html.join('|')})`, 'ig');
+    this.addDependency(dataPath);
     const newSource = insertIncludes(this, source, regex);
 
     return `export default ${JSON.stringify(newSource)}`;
